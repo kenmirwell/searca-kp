0adf44d121d6a063b4186471fc5c0310cc1b4a7d
KenMirwell <kenmirwell@gmail.com>
2025-06-23 16:30:19 +0800

    update

--- includes/components/component.php
+++ includes/components/component.php
@@ -1,30 +1,21 @@
-<?php 
-    $logo_url = get_query_var('logo_url');
-    $card_color = get_query_var('card_color');
-    $aspiring_outcome = get_query_var('aspiring_outcome');
-    $expected_output = get_query_var('expected_output');
-    $thumbnail_url = get_query_var('thumbnail_url');
-    $page_link = get_query_var('page_link');
-    $components_index =  get_query_var('component_index');
-?>
-
 <div class="component-item-element block w-[100%] md:w-auto h-[370px] xl:h-[435px] rounded-[20px] group component-<?php echo $components_index; ?>">
   <div class="flex items-end relative h-[100%] w-[100%] md:w-[250px] xl:w-[400px] rounded-3xl overflow-hidden">
-    <div class="relative p-[20px] z-[2]">
-      <div class="mb-[20px]">
+    <div class="relative p-[20px] z-[2] w-[100%]">
+      <div class="mb-[10px]">
         <div class="text-display-16 xl:text-display-18 font-bold pt-[10px] text-[#ffffff]">
-          <h4><?php the_title() ?></h4>
+          <h4><?php echo esc_html(get_query_var('component_title'));  ?></h4>
         </div>
-        <div id="comp-container-<?php echo get_the_ID(); ?>" class="hidden md:block component-text pt-[10px] font-extralight text-[10px] xl:text-[14px] text-[#ffffff]">
-          <?php echo $aspiring_outcome ?>
+        <div class="hidden md:block component-text pt-[10px] font-extralight text-[10px] xl:text-[14px] text-[#ffffff] pb-[10px] border-b-[1px] border-[#D7D7D7]">
+          <?php echo esc_html(get_query_var('component_1st_sub')); ?>
         </div>
-        <div id="comp-container-<?php echo get_the_ID(); ?>" class="block md:hidden component-text pt-[10px] font-extralight text-[10px] xl:text-[14px] text-[#ffffff]">
-          <?php echo $aspiring_outcome ?>
+        <div class="block md:hidden component-text pt-[10px] font-extralight text-[10px] xl:text-[14px] text-[#ffffff] pb-[10px] border-b-[1px] border-[#D7D7D7]">
+          <?php echo esc_html(get_query_var('component_1st_sub')); ?>
         </div>
       </div>
 
+      <p class="font-extralight text-[10px] xl:text-[14px] text-[#ffffff]"><?php echo esc_html(get_query_var('component_2nd_sub')); ?></p>
       <!-- ✅ Learn More: slide down on card hover, with its own hover effects -->
-      <a class="block w-auto max-h-0 group-hover:max-h-[300px] overflow-hidden transition-all duration-300 ease-in-out delay-150" href="<?php echo esc_url($page_link); ?>">
+      <a class="block w-auto max-h-0 group-hover:max-h-[300px] overflow-hidden transition-all duration-300 ease-in-out delay-150 pt-[10px]" href="<?php echo esc_url(get_query_var('component_button_link')); ?>">
         <div class="flex justify-between items-center gap-[20px] py-[5px] pl-[20px] pr-[5px] bg-[#096936] hover:bg-[#B59637] transition-all duration-200 ease rounded-full">
           <p class="hover:text-[#000000] text-[#ffffff]">Learn More</p>
           <div class="bg-[#B59637] hover:bg-[#096936] rounded-full p-[15px] transition-all duration-200 ease">
@@ -39,8 +30,9 @@
 
     <!-- Image zooms on card hover -->
     <div class="bg-gradient-to-t from-black to-transparent w-[100%] h-[100%] absolute top-0 left-0 z-[1]"></div>
-    <img class="absolute w-full h-full object-cover scale-[1] group-hover:scale-[1.1] transition-all duration-700 ease" src="<?php echo esc_url($thumbnail_url); ?>" alt="<?php the_title(); ?>">
+    <img class="absolute w-full h-full object-cover scale-[1] group-hover:scale-[1.1] transition-all duration-700 ease" src="<?php echo esc_url(get_query_var('component_image')); ?>">
   </div>
 </div>
+
 
 
--- includes/frontpage-sections/key-pillars.php
+++ includes/frontpage-sections/key-pillars.php
@@ -2,44 +2,69 @@
     <div class="w-[90%] lg:w-[1024px] xl:w-[1280px] mx-auto">
         <div class="simple-header flex flex-col gap-[5px] items-baseline text-center w-[100%] mx-auto pb-[20px] md:pb-[40px]">
             <div class="text-left w-[100%] lg:w-[50%]">
-                <h6 class="text-display-24 md:text-display-42 text-[#1f1f1f] pb-[10px] font-bold">Key pillars of our work</h6>
-                <p class="text-display-12 md:text-display-16">The foundation of our work, driving collaboration, research, and sustainable solutions in agriculture, forestry, and natural resource management.</p>
+                <h6 class="text-display-24 md:text-display-42 text-[#1f1f1f] pb-[10px] font-bold">Our Platform Essentials</h6>
+                <p class="text-display-12 md:text-display-16">Driving agricultural transformation and evidence-based policymaking through knowledge, data-driven research, and colloboration across Southeast Asia.</p>
             </div>
         </div>
         <div class="flex flex-wrap xl:flex-nowrap flex-col sm:flex-row justify-start xl:justify-between items-start gap-[5px] lg:gap-[10px] xl:gap-[20px]">
             <?php 
-                $components = new WP_Query(array(
-                    "post_type" => "component",
-                    "posts_per_page" => 10,
-                    'order' => 'DESC',     
-                ));
+                // $components = new WP_Query(array(
+                //     "post_type" => "component",
+                //     "posts_per_page" => 10,
+                //     'order' => 'DESC',     
+                // ));
 
-                if ($components->have_posts()) {
-                    while ($components->have_posts()){
-                        $components->the_post();
-                        $logo_url = get_field("component_logo");
-                        $card_color = get_field("component_color");
-                        $aspiring_outcome = get_field("aspirational_outcome");
-                        $expected_output = get_field("expected_output");
-                        $thumbnail_url = get_the_post_thumbnail_url();
-                        $page_link = get_field("component_page_link");
-                        $components_index = (int) $components->current_post;
+                // if ($components->have_posts()) {
+                //     while ($components->have_posts()){
+                //         $components->the_post();
+                //         $logo_url = get_field("component_logo");
+                //         $card_color = get_field("component_color");
+                //         $aspiring_outcome = get_field("aspirational_outcome");
+                //         $expected_output = get_field("expected_output");
+                //         $thumbnail_url = get_the_post_thumbnail_url();
+                //         $page_link = get_field("component_page_link");
+                //         $components_index = (int) $components->current_post;
 
-                        set_query_var('component_index', $components_index);
-                        set_query_var('logo_url', $logo_url);
-                        set_query_var('card_color', $card_color);
-                        set_query_var('aspiring_outcome', $aspiring_outcome);
-                        set_query_var('expected_output', $expected_output);
-                        set_query_var('thumbnail_url', $thumbnail_url);
-                        set_query_var('page_link', $page_link);
+                //         set_query_var('component_index', $components_index);
+                //         set_query_var('logo_url', $logo_url);
+                //         set_query_var('card_color', $card_color);
+                //         set_query_var('aspiring_outcome', $aspiring_outcome);
+                //         set_query_var('expected_output', $expected_output);
+                //         set_query_var('thumbnail_url', $thumbnail_url);
+                //         set_query_var('page_link', $page_link);
                     
-                    get_template_part("includes/components/component");
+                //     get_template_part("includes/components/component");
                 ?>
-                <?php } } 
+                <?php 
+                //     } } 
                 
-                wp_reset_postdata(); 
+                // wp_reset_postdata(); 
                 
             ?>
+                <?php if (have_rows('component_container')): ?>
+                <!-- <div class="flex flex-col gap-[50px] lg:flex-row justify-between flex-wrap"> -->
+                    <?php while (have_rows('component_container')): the_row(); ?>
+                    <?php
+                            // Get the sub fields
+                            $title = get_sub_field('component_title');
+                            $sub1 = get_sub_field('component_1st_sub');
+                            $sub2 = get_sub_field('component_2nd_sub');
+                            $link = get_sub_field('component_button_link');
+                            $image = get_sub_field('component_image');
+
+                            // Set for template part
+                            set_query_var('component_title', $title);
+                            set_query_var('component_1st_sub', $sub1);
+                            set_query_var('component_2nd_sub', $sub2);
+                            set_query_var('component_button_link', $link);
+                            set_query_var('component_image', $image['url']);
+                    ?>
+                    <div class="flex flex-col items-center gap-[20px]">
+                        <?php get_template_part("includes/components/component"); ?>
+                    </div>
+                    <?php endwhile; ?>
+                <!-- </div> -->
+            <?php endif; ?>
         </div>
     </div>
 </div>
