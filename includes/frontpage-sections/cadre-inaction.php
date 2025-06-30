676e34dbadd426fcf6e4800337b3bd9349dc39bb
KenMirwell <kenmirwell@gmail.com>
2025-06-30 13:28:59 +0800

    udpate

--- includes/frontpage-sections/cadre-inaction.php
+++ includes/frontpage-sections/cadre-inaction.php
@@ -2,26 +2,6 @@
     set_query_var('cadre_in_action_banner', $cadre_in_action_banner);
 ?>
 
-<!-- <div class="flex w-[100%] h-[720px] relative">
-    <div class="flex flex-col xl:flex-row w-[90%] lg:w-[1024px] xl:w-[1280px] justify-center xl:w-[60%] mx-auto gap-[100px] items-center relative z-[9]">
-        <div class="flex flex-col items-center justify-center gap-[20px] group">
-            <div class="flex relative justify-center items-center">
-                <div class="bg-transparent border border-[1px] border-[#ffffff] p-[50px] group-hover:scale-[1.5] rounded-full transition-all duration-200 ease absolute"></div>
-                <div class="bg-[#ffffff] p-[50px] rounded-full"></div>
-                <svg class="z-[2] absolute" width="11" height="14" viewBox="0 0 11 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M0 14V0L11 7L0 14Z" fill="#1D1B20"/>
-                </svg>
-            </div>
-            <h2 class="text-[#ffffff] text-[12px] xl:text-[16px]">Watch the Video</h2>
-        </div>
-    </div>
-    <div class="z-[3] bg-[#ffffff] absolute opacity-[0.05] left-[144px] rotate-[15deg] h-[900px] top-[-80px] w-[146px]"></div>
-    <div class="z-[3] bg-[#ffffff] absolute opacity-[0.03] left-[200px] rotate-[15deg] h-[900px] top-[-80px] w-[146px]"></div>
-    <div class="z-[3] bg-[#ffffff] absolute opacity-[0.05] left-[350px] rotate-[15deg] h-[900px] top-[-80px] w-[146px]"></div>
-    <div class="bg-black opacity-50 w-[100%] h-[100%] absolute top-0 left-0 z-[1]"></div>
-    <img class="absolute w-full h-full object-cover object-bottom" src="<?php //echo esc_url($cadre_in_action_banner) ?>">
-</div> -->
-
 <div id="c-a-video-container" class="flex w-[100%] h-[720px] relative">
     <div class="flex flex-col xl:flex-row w-[90%] lg:w-[1024px] xl:w-[1280px] justify-center xl:w-[60%] mx-auto gap-[100px] items-center relative z-[9]">
         <div id="play-pause-btn" class="flex flex-col items-center justify-center gap-[20px] group">
--- includes/frontpage-sections/capri-section.php
+++ includes/frontpage-sections/capri-section.php
@@ -1,3 +1,7 @@
+<?php 
+  $page = get_query_var('page');
+?>
+
 <div class="relative">
     <div class="bg-[#F2FFF8] overflow-hidden">
       <div class="w-[90%] lg:w-[1024px] xl:w-[1280px] mx-auto">
@@ -7,31 +11,31 @@
             </div>
             <div class="w-[50%] pb-[20px] xl:pt-[50px] flex flex-col gap-[20px] text-left items-left mx-auto z-[2]">
                 <div class="w-[100%]">
-                    <div class="hidden lg:block text-[##1f1f1f] w-[100%] text-display-24 md:text-display-42 font-bold">
-                        <h2>Consortium for Agricultural Policy Research Initiatives (CAPRI)</h2>
-                    </div>
+                     <?php if($page) : ?>
+                        <?php if (get_field("split_section_title", $page->ID)) : ?>
+                            <div class="hidden lg:block text-[##1f1f1f] w-[100%] text-display-24 md:text-display-42 font-bold">
+                                <h2><?php echo get_field("split_section_title", $page->ID); ?></h2>
+                            </div>
+                        <?php endif; ?>
+                    <?php endif; ?>
                     <div class="flex flex-col gap-[20px] pt-[20px]">
                         <div class="text-[#2f2f2f] w-[100%] text-display-12 md:text-display-16">
-                            <p class="">CAPRI is a regional initiative led by the Southeast Asian Regional Center for Graduate Study and Research in Agriculture (SEARCA) to support the sustainable and inclusive transformation of Southeast Asia’s agriculture sector. In light of the region’s rapid urbanization, climate challenges, labor migration, and evolving food security concerns, CAPRI promotes high-impact, evidence-based policy development backed by strategic research, cross-sectoral collaboration, and inclusive engagement.</p>
                             <ul class="flex flex-col w-[100%] gap-[5px] lg:gap-[20px] py-[20px] lg:pt-[30px] agpractices-list">
-                                <li class="flex flex-col items-start gap-[10px]">
-                                  <div class="flex gap-[10px] items-center">
-                                    <svg width="21" height="11" viewBox="0 0 36 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                      <path d="M34 2L12 24L2 14" stroke="#CEAB23" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
-                                    </svg>
-                                    <h6 class="font-[700]">Evidence-Based Research</h6>
-                                  </div>
-                                  <p>Grounded in science and data to guide meaningful agricultural policy reforms.</p>
-                                </li>
-                                <li class="flex flex-col items-start gap-[10px]">
-                                  <div class="flex gap-[10px] items-center">
-                                    <svg width="21" height="11" viewBox="0 0 36 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                      <path d="M34 2L12 24L2 14" stroke="#CEAB23" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
-                                    </svg>
-                                    <h6 class="font-[700]">Collaborative Network</h6>
-                                  </div>
-                                    <p>Connects governments, researchers, and institutions across Southeast Asia.</p>
-                                </li>
+                              <li class="flex flex-col items-start gap-[10px]">
+                                <?php if($page) : ?>
+                                  <?php if (have_rows('split_section_points', $page->ID)) : ?>
+                                    <?php while (have_rows('split_section_points', $page->ID)) : the_row(); ?>
+                                      <div class="flex gap-[10px] items-center">
+                                        <svg width="21" height="11" viewBox="0 0 36 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                          <path d="M34 2L12 24L2 14" stroke="#CEAB23" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
+                                        </svg>
+                                        <h6 class="font-[700]"><?php echo esc_html(get_sub_field('point_title')); ?></h6>
+                                      </div>
+                                      <p><?php echo esc_html(get_sub_field('point_description')); ?></p>
+                                    <?php endwhile; ?>
+                                  <?php endif; ?> 
+                                <?php endif; ?>  
+                              </li>
                             </ul>
                         </div>
                     </div>
