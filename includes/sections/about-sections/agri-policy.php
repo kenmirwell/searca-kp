a30408be461e09890860bb9d36307b47348ddb91
KenMirwell <kenmirwell@gmail.com>
2025-07-07 11:10:39 +0800

    update

--- includes/sections/about-sections/agri-policy.php
+++ includes/sections/about-sections/agri-policy.php
@@ -7,11 +7,11 @@
         <div class="w-[90%] lg:w-[1024px] xl:w-[1280px] mx-auto">
             <div class="flex flex-col relative pt-[50px] py-[60px] justify-center gap-[50px] items-center">
                 <?php if (get_field("agri_policy_title", $page_id)) : ?> 
-                    <div class="hidden lg:block text-[#000000] w-[100%] text-display-24 md:text-display-42 font-bold w-[70%] mr-auto">
+                    <div class="text-[#000000] w-[100%] text-display-28 md:text-display-42 font-bold w-[100%] lg:w-[70%] lg:mr-auto">
                         <h2><?php echo get_field("agri_policy_title", $page_id); ?></h2>
                     </div>
                 <?php endif; ?>    
-                <div class="flex gap-[20px] w-[80%] ml-auto">
+                <div class="flex gap-[20px] w-[100%] lg:w-[80%] lg:ml-auto">
                     <?php if (have_rows('agri_policy_description', $page_id)) : ?>
                         <?php while (have_rows('agri_policy_description', $page_id)) : the_row(); ?>
                             <div class="flex flex-col gap-[20px]">
@@ -34,7 +34,7 @@
                 </div>
                 
                 <?php if (get_field("agri_policy_image", $page_id)) : ?> 
-                    <div class="relative h-[400px] w-[80%] ml-auto">
+                    <div class="relative h-[400px] w-[100%] md:w-[80%] md:ml-auto">
                         <img class="absolute w-full h-full object-cover" src="<?php echo get_field('agri_policy_image'); ?>" alt="searca image">
                     </div>
                 <?php endif; ?>    
