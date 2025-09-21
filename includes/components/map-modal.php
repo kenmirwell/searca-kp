cd5ecc6d9b9ef5870da8235037f518a9e1d178fe
Manuel Kyd Thomas Nagpala <kenmirwell@gmail.com>
2025-09-21 16:15:59 +0800

    update

--- includes/components/map-modal.php
+++ includes/components/map-modal.php
@@ -1,38 +1,18 @@
 <!-- Modal (hidden by default) -->
-<div id="mapModal" 
-     class="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-[9999] hidden">
+<div id="mapModal" class="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-[9999] hidden">
     
+    <!-- Close Button -->
+    <button id="closeModal" 
+            class="absolute top-2 right-2 text-gray-600 hover:text-black text-2xl font-bold">
+        &times;
+    </button>
+
     <!-- Modal content -->
-    <div class="relative w-[90%] h-[90%] p-[50px] bg-white rounded-lg shadow-lg overflow-y-scroll">
-        
-        <!-- Close Button -->
-        <button id="closeModal" 
-                class="absolute top-2 right-2 text-gray-600 hover:text-black text-2xl font-bold">
-            &times;
-        </button>
+    <div class="relative w-[90%] h-[90%] p-[50px] bg-white rounded-lg shadow-lg scrollbar-custom overflow-scroll">
 
-        <!-- Your Content (kept your aspect ratio wrapper) -->
-        <div class="relative w-full" style="aspect-ratio: 4 / 3;">
-            <!-- MAP IMAGES -->
-            <?php 
-                $countries = new WP_Query(array(
-                    "post_type" => "country-profile",
-                    "posts_per_page" => 11,
-                    'order' => 'DESC',     
-                ));
-
-                if ($countries->have_posts()) {
-                    while ($countries->have_posts()) {
-                        $countries->the_post();
-                        $country_index = $countries->current_post;
-                        $image_url = get_field('map');
-            ?>
-                <img id="map-<?php the_title(); ?>" 
-                    class="map-<?php echo $country_index; ?> absolute inset-0 w-full h-full object-contain transition-all duration-300 ease-in-out" 
-                    src="<?php echo esc_url($image_url); ?>" alt="">
-            <?php } } wp_reset_postdata(); ?>
-
-            <!-- <div id="map-container" class="absolute top-0 w-[600px] lg:w-[800px] h-[600px] mx-auto"> -->
+        <div class="w-[100%] h-auto">
+            <div class="fixed-size-map relative">
+                <!-- MAP IMAGES -->
                 <?php 
                     $countries = new WP_Query(array(
                         "post_type" => "country-profile",
@@ -43,31 +23,51 @@
                     if ($countries->have_posts()) {
                         while ($countries->have_posts()) {
                             $countries->the_post();
-                            $country_index = (int) $countries->current_post;
+                            $country_index = $countries->current_post;
+                            $image_url = get_field('map');
+                ?>
+                    <img id="map-<?php the_title(); ?>" 
+                        class="map-<?php echo $country_index; ?> absolute inset-0 w-full h-full object-contain transition-all duration-300 ease-in-out" 
+                        src="<?php echo esc_url($image_url); ?>" alt="">
+                <?php } } wp_reset_postdata(); ?>
 
-                            $flag_url = get_field('flag');
-                ?>
-                    <a href="<?php echo get_permalink(); ?>" 
-                        id="country-<?php the_title(); ?>" 
-                        data-index="<?php echo intval($country_index); ?>" 
-                        class="country-<?php echo $country_index; ?> flex items-center absolute font-[200] transition-all duration-200 ease"
-                    >
-                        <div class="flag-icon w-[100%] relative transition-all duration-300 ease-in-out z-[1]">
-                            <img class="absolute top-[11px] right-[10px] w-[40px] z-[2]" src="<?php echo esc_url($flag_url); ?>" alt="">
-                            <div class="z-[2] w-[60px]">
-                                <svg width="auto" height="auto" viewBox="0 0 54 69" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M53.7712 27.7832C53.7712 27.7832 53.7712 28.039 53.7543 28.4825C53.7543 28.5507 53.7543 28.636 53.7543 28.7042C53.7543 28.7895 53.7543 28.8747 53.7543 28.96V29.0112C53.7543 29.0112 53.7543 29.0964 53.7543 29.1306C53.7543 29.1647 53.7543 29.1988 53.7543 29.2329V29.3011C53.7543 29.5399 53.7206 29.7957 53.6868 30.0856C53.6024 31.1942 53.4674 32.3028 53.248 33.3772C51.7628 42.5016 46.4294 51.4213 27.172 68.5444C8.04965 51.5237 2.6488 42.7404 1.12981 33.5307C1.12981 33.4966 1.12981 33.4625 1.11293 33.4113C0.859769 32.1834 0.70787 30.9043 0.623481 29.6081C0.623481 29.4887 0.623481 29.3523 0.606603 29.2329C0.606603 29.1817 0.606603 29.1135 0.606603 29.0623C0.606603 29.0282 0.606603 28.9941 0.606603 28.943C0.606603 28.8747 0.606603 28.8236 0.606603 28.7383C0.606603 28.7042 0.606603 28.653 0.606603 28.6189V28.4654C0.606603 28.4654 0.606603 28.329 0.606603 28.2608C0.606603 27.9538 0.606603 27.7662 0.606603 27.7662C0.471582 12.9113 12.4041 0.853516 27.1214 0.853516C41.8387 0.853516 53.7712 12.9113 53.7712 27.7832Z" fill="white"/>
-                                </svg>
+                <!-- <div id="map-container" class="absolute top-0 w-[600px] lg:w-[800px] h-[600px] mx-auto"> -->
+                    <?php 
+                        $countries = new WP_Query(array(
+                            "post_type" => "country-profile",
+                            "posts_per_page" => 11,
+                            'order' => 'DESC',     
+                        ));
+
+                        if ($countries->have_posts()) {
+                            while ($countries->have_posts()) {
+                                $countries->the_post();
+                                $country_index = (int) $countries->current_post;
+
+                                $flag_url = get_field('flag');
+                    ?>
+                        <a href="<?php echo get_permalink(); ?>" 
+                            id="fixed-country-<?php the_title(); ?>" 
+                            data-index="<?php echo intval($country_index); ?>" 
+                            class="country-<?php echo $country_index; ?> flex items-center absolute font-[200] transition-all duration-200 ease"
+                        >
+                            <div class="flag-icon w-[100%] relative transition-all duration-300 ease-in-out z-[1]">
+                                <img class="absolute top-[11px] right-[10px] w-[40px] z-[2]" src="<?php echo esc_url($flag_url); ?>" alt="">
+                                <div class="z-[2] w-[60px]">
+                                    <svg width="auto" height="auto" viewBox="0 0 54 69" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M53.7712 27.7832C53.7712 27.7832 53.7712 28.039 53.7543 28.4825C53.7543 28.5507 53.7543 28.636 53.7543 28.7042C53.7543 28.7895 53.7543 28.8747 53.7543 28.96V29.0112C53.7543 29.0112 53.7543 29.0964 53.7543 29.1306C53.7543 29.1647 53.7543 29.1988 53.7543 29.2329V29.3011C53.7543 29.5399 53.7206 29.7957 53.6868 30.0856C53.6024 31.1942 53.4674 32.3028 53.248 33.3772C51.7628 42.5016 46.4294 51.4213 27.172 68.5444C8.04965 51.5237 2.6488 42.7404 1.12981 33.5307C1.12981 33.4966 1.12981 33.4625 1.11293 33.4113C0.859769 32.1834 0.70787 30.9043 0.623481 29.6081C0.623481 29.4887 0.623481 29.3523 0.606603 29.2329C0.606603 29.1817 0.606603 29.1135 0.606603 29.0623C0.606603 29.0282 0.606603 28.9941 0.606603 28.943C0.606603 28.8747 0.606603 28.8236 0.606603 28.7383C0.606603 28.7042 0.606603 28.653 0.606603 28.6189V28.4654C0.606603 28.4654 0.606603 28.329 0.606603 28.2608C0.606603 27.9538 0.606603 27.7662 0.606603 27.7662C0.471582 12.9113 12.4041 0.853516 27.1214 0.853516C41.8387 0.853516 53.7712 12.9113 53.7712 27.7832Z" fill="white"/>
+                                    </svg>
+                                </div>
                             </div>
-                        </div>
-                        <p class="absolute w-max z-[0] text-[14px] font-[500] flex justify-start items-center py-[7px] ml-[30px] rounded-br-full rounded-tr-full  bg-[#ffffff] overflow-hidden transition-all duration-300 ease-in-out"><?php the_title(); ?></p>
-                    </a>
-                <?php 
+                            <p class="absolute w-max z-[0] text-[14px] font-[500] flex justify-start items-center py-[7px] ml-[30px] rounded-br-full rounded-tr-full  bg-[#ffffff] overflow-hidden transition-all duration-300 ease-in-out"><?php the_title(); ?></p>
+                        </a>
+                    <?php 
+                        } 
                     } 
-                } 
-                wp_reset_postdata(); // Reset the query
-                ?>
-            <!-- </div> -->
+                    wp_reset_postdata(); // Reset the query
+                    ?>
+                <!-- </div> -->
+            </div>
         </div>
     </div>
 </div>
